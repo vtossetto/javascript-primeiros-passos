9a1d7a8025f4218d56923b02c8e9d627183f884e
Vanessa Tossetto <vanessa.tossetto@somos.interno>
2022-01-24 21:22:29 -0300

    commit inicial

---
+++ 2-trabalhandoComVariaveis.js
@@ -0,0 +1,11 @@
+//Console.log("Trabalhando com Variáveis");
+console.log("Trabalhando com Variáveis");
+//JS é case sensitive
+
+const idade = 29;
+const nome = "Ricardo";
+let ano = 2020;
+
+//não fazer
+//mes = "Janeiro"
+//usar const 
---
+++ 5-atribuicaoDeVariaveis.js
@@ -0,0 +1,20 @@
+console.log("Trabalhando com Atribuição de Variáveis");
+
+
+const primeiroNome = "Ricardo";
+const sobrenome = "Bugan";
+
+//console.log(nome + " " + sobrenome);
+console.log(primeiroNome, sobrenome);
+console.log(`Men nome é ${primeiroNome} ${sobrenome}`);
+
+let contador = 0;
+contador = contador + 1;
+
+const nomeCompleto = primeiroNome + sobrenome;
+console.log(nomeCompleto);
+
+let idade; //declarando variável
+idade = 26; //atribuindo valor
+idade = idade +1;
+console.log(idade);
